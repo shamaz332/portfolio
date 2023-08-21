7360e943a94b0b5b916d3344750c1d853a00db97
Shamaz saeed <shamazsaeed332@gmail.com>
2023-08-21 19:03:54 +0500

    update portfolio

--- components/About.jsx
+++ components/About.jsx
@@ -1,17 +1,51 @@
-import { gitHubUrl, projectsUrl } from '../constants';
+/* eslint-disable react/no-unescaped-entities */
+import { gitHubUrl, projectsUrl } from '../constants'
 
-import Link from 'next/link';
-import styles from '../styles/About.module.css';
+import Link from 'next/link'
+import styles from '../styles/About.module.css'
 
 export default function About() {
-    return (
-        <div className={styles.aboutMe}>
-            <h2>About Me</h2>
-            <div className={styles.desc}>
-                <p>Hi there! My name is Shamaz Saeed, I build and develop real-time web applications mainly in HTML5, CSS3, JavaScript, React, Vue, Firebase, etc.</p>
-                <p>I always try to learn about new technologies and create high-quality projects in my spare time!</p>
-                <p>My <Link href={projectsUrl}><a target="_blank">projects</a></Link> are available on my <Link href={gitHubUrl}><a target="_blank">GitHub profile</a></Link>. Feel free to check out my resume or contact me by email!</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className={styles.aboutMe}>
+			<h2>About Me</h2>
+			<div className={styles.desc}>
+				<p>
+					I'm Shamaz Saeed, a skilled developer specializing in
+					real-time web applications. With expertise spanning
+					JavaScript, HTML, CSS, Redux, Material-UI, React, and more,
+				</p>
+				<p>
+					I create seamless user experiences. My proficiency in
+					technologies like Gatsby, Bootstrap, and GraphQL ensures
+					visually appealing and responsive interfaces.
+				</p>
+				<p>
+					I excel in optimizing performance, using Google Analytics,
+					SEO, and responsive design. Leveraging Node.js and MongoDB,
+				</p>
+				<p>
+					I handle backend intricacies while also excelling in
+					ElectronJS for cross-platform desktop apps. Git, GitHub,
+					JIRA, and agile methodologies streamline my collaborative
+					workflow.{' '}
+				</p>
+				<p>
+					<p>
+						Testing frameworks like Jest and Storybook ensure code
+						quality. I'm experienced in CI/CD and skilled with
+						GraphQL and Contentful.
+					</p>
+					My{' '}
+					<Link href={projectsUrl}>
+						<a target='_blank'>projects</a>
+					</Link>{' '}
+					are available on my{' '}
+					<Link href={gitHubUrl}>
+						<a target='_blank'>GitHub profile</a>
+					</Link>
+					. Feel free to check out my resume or contact me by email!
+				</p>
+			</div>
+		</div>
+	)
 }
--- constants.js
+++ constants.js
@@ -11,12 +11,12 @@ export const projectsUrl = gitHubUrl + '?tab=repositories';
 export const linkedinUrl = 'https://www.linkedin.com/in/shamaz-saeed';
 
 const localResume = '/resume/ShamazResume.pdf';
-const gDocsResume = 'https://docs.google.com/viewer?url=' + remoteServer + localResume + '&embedded=true';
+const gDocsResume = 'https://drive.google.com/file/d/1bJ7cRUPNPtLKdBK5NP73wHPQYfBzPSFM/view?usp=sharing';
 export const resumeUrl = devEnv ? localResume : gDocsResume;
 
 export const pageAuthor = 'Shamaz Saeed';
-export const pageTitle = pageAuthor + ' | Frontend Dev';
-export const pageDesc = 'Hi there! I am Shamaz Saeed, a Frontend Developer at NSTOP Group AB. Check out my projects on GitHub! - @shamaz332';
+export const pageTitle = pageAuthor + ' | Software Developer';
+export const pageDesc = 'Hi there! I am Shamaz Saeed, a Software Developer. Check out my projects on GitHub! - @shamaz332';
 export const pageTheme = '#1A3A59';
 
 export const avatarUrl = '/img/profile.png';
